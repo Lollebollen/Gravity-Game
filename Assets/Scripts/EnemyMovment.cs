e6bcbcbd9aefb8bd6f83c01e926a71b7c6ee347f
SenseiLegacy <danne._boy@hotmail.com>
2024-09-25 08:50:57 +0200

    groundmovment script change and animations and prefabs changes

--- Assets/Scripts/EnemyMovment.cs
+++ Assets/Scripts/EnemyMovment.cs
@@ -9,14 +9,18 @@ public class EnemyMovment : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
      SpriteRenderer spriteRenderer;
 
-    private float gravity = -2;
-
+    private float gravity = 2;
+    public bool isGroundEnemy;
     
 
 
     void Start()
     {   
         spriteRenderer = GetComponent<SpriteRenderer>();
+        if (isGroundEnemy)
+        {
+            gravity = -2;
+        }
         while(x ==0)
         {
             x = Random.Range(-1,1);   
@@ -26,14 +30,19 @@ public class EnemyMovment : MonoBehaviour
 
     void Update()
     {
+
+        if (rb.velocity.x < 0.1 && rb.velocity.x > -0.1)
+        {
+            x *= -1;
+        }
         rb.velocity = new Vector2(x*enemyMoveSpeed, gravity);
         Flip(); 
     }
 
-    private void OnCollisionEnter2D()
-    {
-        x *= -1;
-    }
+    // private void OnCollisionEnter2D()
+    // {
+    //     x *= -1;
+    // }
 
     private void Flip()
     {
